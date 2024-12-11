2903aa1175d707bd2e9d6a8666d2d51f7b1c0bb5
Полина Платонова <platonovaPE@outlook.com>
2024-12-11 18:14:43 +0500

    Task6

--- Tyuiu.PlatonovaPE.Sprint6.Task6.V26.Lib/DataService.cs
+++ Tyuiu.PlatonovaPE.Sprint6.Task6.V26.Lib/DataService.cs
@@ -1,32 +1,27 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint6;
+using System.IO;
 namespace Tyuiu.PlatonovaPE.Sprint6.Task6.V26.Lib
 {
     public class DataService : ISprint6Task6V26
     {
-
-
-        public string CollectTextFromFile(string path)
-        {
-            string resStr = "";
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    string[] mas = line.Split(' ');
-                    for (int i = 0; i < mas.Length - 1; i++)
-                    {
-                        resStr += mas[mas.Length - 2] + " ";
-                        break;
-                    }
-                }
-            }
-            return resStr.Trim();
-        }
-
         public string CollectTextFromFile(string str, string path)
         {
-            throw new NotImplementedException();
+            {
+                string resStr = "";
+
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var temp = line.Split(' ');
+
+                        resStr += temp[^1] + " ";
+                    }
+                    return resStr;
+                }
+            }
         }
     }
 }
